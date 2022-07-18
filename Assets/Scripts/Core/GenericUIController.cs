ce0445f3e6cdebafa2924dabfccfefc81a53e496
Benjamin Lyon <benlyon1994@gmail.com>
2022-07-19 00:58:48 +0100

    Appended OpenScene to allow for sound to finish before changing scenes.
    Also shortened the sound and added generic click to all buttons.

--- Assets/Scripts/Core/GenericUIController.cs
+++ Assets/Scripts/Core/GenericUIController.cs
@@ -32,6 +32,15 @@ public class GenericUIController : MonoBehaviour
      */
     public void OpenScene(string sceneName)
     {
+        StartCoroutine(StartOpenRoutine(sceneName));
+    }
+
+    /**
+     * Put our delay in for opening the scene for if there's any menu UI. Input sound should finish.
+     */
+    IEnumerator StartOpenRoutine(string sceneName)
+    {
+        yield return new WaitWhile(() => menuAudio.isPlaying);
         SceneManager.LoadScene(sceneName);
     }
 }
